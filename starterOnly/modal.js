2fd38b5094070312162bcc8db7d8b8db71091269
leolegrand <88097360+leolegrand@users.noreply.github.com>
2021-09-29 14:43:57 +0200

    Update modal.js
    The (x) button now closes the modal when clicked

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Pour chaque itération de closeBtn, j'applique closeModal si un clique est effectué
+closeBtn.forEach((xBtn) => xBtn.addEventListener("click", closeModal));
 
+// Fais disparaitre modalbg
+function closeModal() {
+  modalbg.style.display = "none";
+}
